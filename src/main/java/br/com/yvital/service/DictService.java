3053cef10d2659d0e5285ac5e6f4ebe9ea11aa5c
Francisco Yuri Vital Silva <yuri@MacBook-Pro-de-Francisco.local>
2025-03-24 15:54:17 -0300

    Aula sobre redis

--- src/main/java/br/com/yvital/service/DictService.java
+++ src/main/java/br/com/yvital/service/DictService.java
@@ -1,12 +1,16 @@
 package br.com.yvital.service;
 
+import br.com.yvital.config.RedisCache;
 import br.com.yvital.model.Chave;
 import br.com.yvital.model.TipoChave;
 import br.com.yvital.model.TipoPessoa;
+import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @ApplicationScoped
 public class DictService {
@@ -19,6 +23,26 @@ public class DictService {
     private String cnpj;
     @ConfigProperty(name = "pix.nome")
     private String nome;
+
+    @Inject
+    RedisCache redisCache;
+
+    public Chave buscarDetalhesChave(String key){
+        var chave = buscarChaveCache(key);
+        if (Objects.isNull(chave)){
+            var chaveFake = buscarChave(key);
+            redisCache.set(key, buscarChave(key));
+            return chaveFake;
+        }
+
+        return chave;
+    }
+
+    private Chave buscarChaveCache(String key){
+        var chave = redisCache.get(key);
+        Log.infof("Chave encontrada no cache %s", chave);
+        return chave;
+    }
 
     public Chave buscarChave(String chave) {
         return  new Chave(
